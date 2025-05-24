6a273315fe4dd9d065e379be5e8e76f2f485ef30
sheona001 <sirexcc001@gmail.com>
2025-05-24 14:11:39 +0600

    prefences

--- server/Routes/VetRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getNearbyVets } = require("../Controller/VetController");
-const router = express.Router();
-
-// New route to fetch nearby vets
-router.get("/nearby-vets", getNearbyVets);
-
-module.exports = router;
