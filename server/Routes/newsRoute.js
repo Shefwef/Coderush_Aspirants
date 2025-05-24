a078b61a4214097f4839a8dff824f73642b1ff69
sheona001 <sirexcc001@gmail.com>
2025-05-24 12:19:07 +0600

    auth

--- server/Routes/newsRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { fetchPetNews } = require("../Controller/newsController");
-const router = express.Router();
-
-router.get("/news", fetchPetNews);
-
-module.exports = router;
