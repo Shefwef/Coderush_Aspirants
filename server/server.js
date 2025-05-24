87be37bf9b24c4d685beb4f7b50cab8297e0c3fc
Nazifa Tasneem <112499963+nazifatasneem13@users.noreply.github.com>
2025-05-24 12:30:00 +0600

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,82 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const petRouter = require("./Routes/PetRoute");
+const AdoptFormRoute = require("./Routes/AdoptFormRoute");
+const userRoute = require("./Routes/user.route");
+const authRoute = require("./Routes/auth.route");
+const AdminRoute = require("./Routes/AdminRoute");
+const vetRouter = require("./Routes/VetRoute");
+const wishlistRouter = require("./Routes/wishlist");
+const communicationRouter = require("./Routes/ChatRoutes");
+const profilerouter = require("./Routes/profileroute.js");
+const msgRouter = require("./Routes/MessageRoutes");
+const trainRoute = require("./Routes/TrainRoute");
+const notifyroutes = require("./Routes/notifyroutes");
+const newsRoutes = require("./Routes/newsRoute.js");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const createError = require("./utils/createError");
+
+const app = express();
+
+app.use(morgan("combined"));
+
+app.use(helmet());
+
+const allowedOrigins = ["https://wnco.onrender.com", "http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  })
+);
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(petRouter);
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
+app.use("/api/vets", vetRouter);
+app.use("/form", AdoptFormRoute);
+app.use("/admin", AdminRoute);
+app.use("/train", trainRoute);
+app.use("/wishlist", wishlistRouter);
+app.use("/profile", profilerouter);
+app.use("/chats", communicationRouter);
+app.use("/messages", msgRouter);
+app.use("/api", newsRoutes);
+app.use("/notifications", notifyroutes);
+
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB Atlas:", err);
+  });
+
+app.all("*", (req, res, next) => {
+  next(createError(404, `Cannot find ${req.originalUrl} on this server!`));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res
+    .status(err.status || 500)
+    .json({ error: err.message || "Something went wrong!" });
+});
